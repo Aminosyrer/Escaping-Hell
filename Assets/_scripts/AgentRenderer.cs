50a68b1adf1920bcc484cd1d2a53392b90b0146c
fhs15 <39727804+fhs15@users.noreply.github.com>
2022-10-10 14:37:28 +0200

    Finished

--- Assets/_scripts/AgentRenderer.cs
+++ Assets/_scripts/AgentRenderer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,15 +13,13 @@ public class AgentRenderer : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    public void FaceDirection(Vector2 pointerinput)
+    public void FaceDirection(int PointerArea)
     {
-        var direction = (Vector3)pointerinput - transform.position;
-        var result = Vector3.Cross(Vector2.up, direction);
-        if(result.z > 0)
+        if(PointerArea == 0)
         {
             spriteRenderer.flipX = true;
         }
-        else if(result.z < 0)
+        else
         {
             spriteRenderer.flipX = false;
         }
