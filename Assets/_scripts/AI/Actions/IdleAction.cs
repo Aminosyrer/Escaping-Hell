43838b57a40f965c98a0b4b9d45e2f9b5c6732cf
Fhs15 <janus.o@live.dk>
2022-11-20 18:11:48 +0100

    removed agentmovement from monster

--- Assets/_scripts/AI/Actions/IdleAction.cs
+++ Assets/_scripts/AI/Actions/IdleAction.cs
@@ -6,7 +6,6 @@ public class IdleAction : AIAction
 {
     public override void TakeAction()
     {
-        Debug.Log(this.transform.parent.gameObject.transform.parent.gameObject.name);
         enemyBrain.NavHandler.ClearTarget();
         aiMovementData.Direction = Vector2.zero;
         aiMovementData.PointOfInterest = transform.position;
