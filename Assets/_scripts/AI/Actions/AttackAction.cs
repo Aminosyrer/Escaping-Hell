74038a93c5d0320b0504c3606abe22390fd313d7
Fhs15 <janus.o@live.dk>
2022-11-22 00:19:01 +0100

    Fixed bullet clipping and changed bahavior of attacks

--- Assets/_scripts/AI/Actions/AttackAction.cs
+++ Assets/_scripts/AI/Actions/AttackAction.cs
@@ -6,6 +6,8 @@ public class AttackAction : AIAction
 {
     public override void TakeAction()
     {
+        Debug.Log("hitting player");
+        enemyBrain.NavHandler.ClearTarget();
         aiMovementData.Direction = Vector2.zero;
         aiMovementData.PointOfInterest = enemyBrain.Target.transform.position;
         enemyBrain.Move(aiMovementData.Direction, aiMovementData.PointOfInterest);
